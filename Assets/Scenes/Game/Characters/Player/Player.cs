866d768bd77e8ccdc2c4086b5ea19c4395f2007d
p.rurka <pawel.rurka.stud@pw.edu.pl>
2024-01-18 23:57:04 +0100

    Added Spell Cooldown (In Player sheet)

--- Assets/Scenes/Game/Characters/Player/Player.cs
+++ Assets/Scenes/Game/Characters/Player/Player.cs
@@ -15,6 +15,15 @@ namespace Assets.Scenes.Game
         private Movement _movement;
         private Weapon _weapon;
         private Rigidbody2D _body;
+        public float qCooldown;
+        public float eCooldown;
+        public float rCooldown;
+        private float qCooldown_T;
+        private float eCooldown_T;
+        private float rCooldown_T;
+        private bool qReady = true;
+        private bool eReady = true;
+        private bool rReady = true;
         public TextMeshProUGUI healthText;
 
         // Start is called before the first frame update
@@ -29,8 +38,10 @@ namespace Assets.Scenes.Game
             _body = GetComponent<Rigidbody2D>();
 
             _weapon.InvokeRepeating("DoFireBolt", 0.0f, _weapon.FireRate);
+            InvokeRepeating("UpdateQ", 1f, 1f);
+            InvokeRepeating("UpdateE", 1f, 1f);
+            InvokeRepeating("UpdateR", 1f, 1f);
 
-            
 
         }
 
@@ -38,19 +49,26 @@ namespace Assets.Scenes.Game
         void Update()
         {
 
-            if (Input.GetKeyDown("q"))
+            if (Input.GetKeyDown("q") && qReady)
             {
                 _weapon.Invoke("DoShuriken", 0.0f);
+                qReady = false;
+                qCooldown_T = qCooldown;
+
             }
 
-            if (Input.GetKeyDown("e"))
+            if (Input.GetKeyDown("e") && eReady)
             {
                 _weapon.Invoke("DoRocketBolt", 0.0f);
+                eReady = false;
+                eCooldown_T = eCooldown;
             }
 
-            if (Input.GetKeyDown("r"))
+            if (Input.GetKeyDown("r") && rReady)
             {
                 _weapon.Invoke("DoFog", 0.0f);
+                rReady = false;
+                rCooldown_T = rCooldown;
             }
 
             if (HealthPoints > 0)
@@ -75,6 +93,64 @@ namespace Assets.Scenes.Game
             _body.bodyType = RigidbodyType2D.Static;
             _anim.SetTrigger("die");
             Invoke("Die", 0.3f);
+        }
+
+
+        public void UpdateQ()
+        {
+            if (qReady==false)
+            {
+                if(qCooldown_T<= 0)
+                {
+                    qReady = true;
+                }
+                else
+                {
+                    qCooldown_T--;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public void UpdateE()
+        {
+            if (eReady == false)
+            {
+                if (eCooldown_T <= 0)
+                {
+                    eReady = true;
+                }
+                else
+                {
+                    eCooldown_T--;
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+        public void UpdateR()
+        {
+            if (rReady == false)
+            {
+                if (rCooldown_T <= 0)
+                {
+                    rReady = true;
+                }
+                else
+                {
+                    rCooldown_T--;
+                }
+            }
+            else
+            {
+                return;
+            }
         }
 
         public void Die()
